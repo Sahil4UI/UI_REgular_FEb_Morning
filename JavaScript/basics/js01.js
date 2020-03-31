7884028693b8cec6cc159137c938371388de143f
Sahil4UI <54834362+Sahil4UI@users.noreply.github.com>
2020-03-31 11:03:10 +0530

    Add files via upload

---
+++ JavaScript/basics/js01.js
@@ -0,0 +1,8 @@
+function show()
+{
+    textBox  = document.getElementById("box");
+    span= document.getElementById("result");
+    span.innerText = textBox.value;
+    // console.log(textBox.value);
+
+}
